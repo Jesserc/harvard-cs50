4ff66339006062f9248e5abc62c70ccb9f7c7aff
Jesserc <raymondjesse713@gmail.com>
2023-03-14 13:32:48 +0100

    ascii table

---
+++ Arrays & Strings In Memory/strings.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+/*
+ strings are arrays of char (single characters)
+and char(s) are also numbers based on ASCII representation
+ */
+
+int main(void)
+{
+}
